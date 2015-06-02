3c6b3f55aad0038dd36868b9bbbf58f86733eace
wxq <25199344@qq.com>
2015-06-03 07:57:23 +0800

    install mantis

--- installmantis.sh
+++ installmantis.sh
@@ -1,7 +1,8 @@
 sudo docker pull tutum/lamp
-sudo mkdir /var/xiaheng
-sudo mkdir /var/xiaheng/mysql
-sudo mkdir /var/xiaheng/www
-sudo mkdir /var/xiaheng/www/mantis
-sudo cp -r mantis/* /var/xiaheng/www/mantis
-sudo docker run -d -p 80:80 -p 3306:3306 -v /var/xiaheng/mysql:/var/lib/mysql -v /var/xiaheng/www:/app tutum/lamp
+sudo mkdir /var/yourcompanyname
+sudo mkdir /var/yourcompanyname/mysql
+sudo mkdir /var/yourcompanyname/www
+sudo mkdir /var/yourcompanyname/www/mantis
+sudo cp -r mantis/* /var/yourcompanyname/www/mantis
+sudo chmod -R 777 /var/yourcompanyname
+sudo docker run -d -p 80:80 -p 3306:3306 -v /var/yourcompanyname/mysql:/var/lib/mysql -v /var/yourcompanyname/www:/app tutum/lamp
